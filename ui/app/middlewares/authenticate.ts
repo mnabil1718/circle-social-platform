a81dded786db6e19dc301c0dca61dd2d2a0fb5f4
mnabil1718 <mnabil1718@gmail.com>
2026-03-07 12:07:56 +0700

    fix auth not working on SPA

--- ui/app/middlewares/authenticate.ts
+++ ui/app/middlewares/authenticate.ts
@@ -3,13 +3,16 @@ import { redirect } from "react-router";
 import { toastError } from "~/utils/toast";
 
 export async function authenticateMiddleware({ context }: { context: any }, next: any) {
+    if (typeof window === "undefined") {
+        await next();
+        return;
+    }
+
     const state = store.getState();
     const user = state.auth.user;
-
     if (!user) {
         toastError("You have to log in first");
         throw redirect("/login");
     }
-
     await next();
 }
