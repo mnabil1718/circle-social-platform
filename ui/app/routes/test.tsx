30cfb2317c391af00010a4996e5cbb5dbaf84a77
mnabil1718 <mnabil1718@gmail.com>
2026-03-07 16:13:56 +0700

    fix: redirect on /

--- ui/app/routes/test.tsx
+++ ui/app/routes/test.tsx
@@ -1,3 +1,12 @@
+import { redirect } from "react-router";
+import { store } from "~/store/store";
+import type { Route } from "./+types/test";
+
+export async function clientLoader() {
+    const user = store.getState().auth.user;
+    throw redirect(user ? "/home" : "/login");
+}
+
 export default function Test() {
-    return <div>Test</div>
+    return null; // never renders
 }
