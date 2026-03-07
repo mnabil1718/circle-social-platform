fbf46625660a16675ce3901b9fa8ca8aabcfda63
mnabil1718 <mnabil1718@gmail.com>
2026-03-07 12:15:02 +0700

    fix god knows what

--- ui/app/store/auth.tsx
+++ ui/app/store/auth.tsx
@@ -4,52 +4,54 @@ import type { RootState } from "./store";
 import { updateProfile } from "./profile";
 
 export interface AuthState {
-  user: UserLoginResponse | null;
+    user: UserLoginResponse | null;
 }
 
 const LOCAL_STORAGE_KEY = "store:auth";
 
 // local storage
 export function loadAuthFromStorage() {
-  if (typeof window === "undefined") return null;
-  const item = localStorage.getItem(LOCAL_STORAGE_KEY);
-  if (item) {
-    return JSON.parse(item);
-  }
-  return null;
+    if (typeof window === "undefined") return null;
+    try {
+        const item = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (!item || item === "undefined") return null;
+        return JSON.parse(item);
+    } catch {
+        return null;
+    }
 }
 
 const initialState: AuthState = {
-  user: loadAuthFromStorage(),
+    user: loadAuthFromStorage(),
 };
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    login(state, action: PayloadAction<UserLoginResponse>) {
-      state.user = action.payload;
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(action.payload));
-    },
-    logout(state) {
-      if (typeof window !== "undefined")
-        localStorage.removeItem(LOCAL_STORAGE_KEY);
+    name: "auth",
+    initialState,
+    reducers: {
+        login(state, action: PayloadAction<UserLoginResponse>) {
+            state.user = action.payload;
+            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(action.payload));
+        },
+        logout(state) {
+            if (typeof window !== "undefined")
+                localStorage.removeItem(LOCAL_STORAGE_KEY);
 
-      // returning initial state will get data from
-      // localStorage and user will never be null
-      state.user = null;
+            // returning initial state will get data from
+            // localStorage and user will never be null
+            state.user = null;
+        },
     },
-  },
-  extraReducers(builder) {
-    builder.addCase(updateProfile.fulfilled, (state, action) => {
-      const { name, username, avatar } = action.payload;
-      if (state.user) {
-        state.user.name = name;
-        state.user.username = username;
-        if (avatar) state.user.avatar = avatar;
-      }
-    });
-  },
+    extraReducers(builder) {
+        builder.addCase(updateProfile.fulfilled, (state, action) => {
+            const { name, username, avatar } = action.payload;
+            if (state.user) {
+                state.user.name = name;
+                state.user.username = username;
+                if (avatar) state.user.avatar = avatar;
+            }
+        });
+    },
 });
 
 export const { login, logout } = authSlice.actions;
