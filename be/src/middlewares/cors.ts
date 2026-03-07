b58fb39484c7e71ccd5a282982267db317a68795
mnabil1718 <mnabil1718@gmail.com>
2026-03-07 11:02:18 +0700

    chore: remove unused cors origin

--- be/src/middlewares/cors.ts
+++ be/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import { config } from "../utils/config.js";
 
 export const corsMiddleware = cors({
-    origin: [`${config.host}:${config.port}`, config.frontend_url],
+    origin: [config.frontend_url],
     credentials: true,
     optionsSuccessStatus: 200,
 });
