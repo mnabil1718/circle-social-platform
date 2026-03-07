bf948889ed12d5f150079e437f0e9be74352c116
mnabil1718 <mnabil1718@gmail.com>
2026-03-07 14:16:27 +0700

    test env

--- ui/app/root.tsx
+++ ui/app/root.tsx
@@ -1,10 +1,10 @@
 import {
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    isRouteErrorResponse,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "react-router";
 import { Toaster } from "~/components/ui/sonner";
 import { Provider } from "react-redux";
@@ -18,86 +18,87 @@ import { AuthWatcher } from "./components/auth/auth-watch";
 import { PageLoading } from "./components/page-loading";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+    },
+    {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    initSockets();
+    useEffect(() => {
+        initSockets();
 
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body suppressHydrationWarning className="dark">
-        <Provider store={store}>
-          {children}
-          <Toaster duration={5000} />
-          <ScrollRestoration />
-          <Scripts />
-        </Provider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body suppressHydrationWarning className="dark">
+                <Provider store={store}>
+                    {children}
+                    <Toaster duration={5000} />
+                    <ScrollRestoration />
+                    <Scripts />
+                </Provider>
+            </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return (
-    // Place auth watcher inside toaster provider
-    // to be able to navigate and show toast message
-    <AuthWatcher>
-      <Outlet />
-    </AuthWatcher>
-  );
+    console.log("Vite Env Check:", import.meta.env);
+    return (
+        // Place auth watcher inside toaster provider
+        // to be able to navigate and show toast message
+        <AuthWatcher>
+            <Outlet />
+        </AuthWatcher>
+    );
 }
 
 export function HydrateFallback() {
-  return <PageLoading />;
+    return <PageLoading />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
+    let message = "Oops!";
+    let details = "An unexpected error occurred.";
+    let stack: string | undefined;
 
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
+    if (isRouteErrorResponse(error)) {
+        message = error.status === 404 ? "404" : "Error";
+        details =
+            error.status === 404
+                ? "The requested page could not be found."
+                : error.statusText || details;
+    } else if (import.meta.env.DEV && error && error instanceof Error) {
+        details = error.message;
+        stack = error.stack;
+    }
 
-  return (
-    <main className="pt-16 p-4 container mx-auto bg-background">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
+    return (
+        <main className="pt-16 p-4 container mx-auto bg-background">
+            <h1>{message}</h1>
+            <p>{details}</p>
+            {stack && (
+                <pre className="w-full p-4 overflow-x-auto">
+                    <code>{stack}</code>
+                </pre>
+            )}
+        </main>
+    );
 }
